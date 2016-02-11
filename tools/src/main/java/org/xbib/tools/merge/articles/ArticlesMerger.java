c09884d26a8912d364f8bab034db9e627886b6e4
Jörg Prante <joergprante@gmail.com>
2016-02-11 14:37:55 +0100

    next snapshot

--- tools/src/main/java/org/xbib/tools/merge/articles/ArticlesMerger.java
+++ tools/src/main/java/org/xbib/tools/merge/articles/ArticlesMerger.java
@@ -33,7 +33,6 @@ package org.xbib.tools.merge.articles;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.elasticsearch.action.admin.cluster.health.ClusterHealthStatus;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
