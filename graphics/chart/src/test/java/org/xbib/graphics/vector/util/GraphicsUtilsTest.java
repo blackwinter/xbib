c895af2bd1e55f5f321acd20489da1a656229b1a
Jörg Prante <joergprante@gmail.com>
2016-02-19 14:43:14 +0100

    next snapshot

--- graphics/chart/src/test/java/org/xbib/graphics/vector/util/GraphicsUtilsTest.java
+++ graphics/chart/src/test/java/org/xbib/graphics/vector/util/GraphicsUtilsTest.java
@@ -24,7 +24,9 @@ import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
-
+/**
+ * On Linux, the package msttcorefonts need to be installed
+ */
 public class GraphicsUtilsTest {
     private static final double DELTA = 1e-15;
 
